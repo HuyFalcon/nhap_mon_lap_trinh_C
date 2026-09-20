a0d99d3d563bd94ea72224f449200a9c37206643
HuyFalcon <huy2543ck@gmail.com>
2026-09-20 22:04:57 +0700

    Add files via upload

---
+++ bai4_printf_scanf.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+// khai bao 3 bien cung 1 kieu du lieu 
+//int main(){
+//	
+//	int a = 1 , b=2 , c=3 ;
+//	printf("%d \n%d \n%d", a ,b ,c );
+//}
+//
+// //su dung ham scanf
+//int main(){
+//	int a ;
+//	printf("nhap gia tri a : ");
+//	scanf("%d",&a);
+//	printf("gia tri ban vua nhap la : %d",a );
+//}
+//// nang cao hon 
+//int main(){
+//	int a , b , c;
+//	printf("nhap gia tri a:");
+//	scanf("%d",&a);
+//	printf("nhap gia tri b:");
+//	scanf("%d", &b);
+//	printf("nhap gia tri c : ");
+//	scanf("%d",&c);
+//	printf("gia tri lien 3 so la : \n%d \n%d \n%d",a,b,c);
+//}
+
+int main(){
+	int a , b , c;
+	printf("nhap tuoi cua ban:");
+	scanf("%d",&a);
+	printf("nhap chieu cao cua ban:");
+	scanf("%d", &b);
+	printf("nhap ma sinh vien cua ban: ");
+	scanf("%d",&c);
+	printf("toi nam nay: %d tuoi \ntoi cao:%d cm , \nma sinh vien cua toi: %d",a,b,c);
+}
+
+
+
+
+
