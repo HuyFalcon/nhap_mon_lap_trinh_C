e8aa152db867ca4308829ac3b7343808191e2580
HuyFalcon <huy2543ck@gmail.com>
2026-09-20 22:03:33 +0700

    Add files via upload

---
+++ b2_khai_bao_bien.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+//int: %d
+//long long : %lld
+//float : %f
+//double : %lf
+//char : %c
+
+int main(){
+    int a = 100;
+    int b = 50;
+    printf("gia tri cua bien a la: %d , %d " , b , a );
+    printf("\n");
+
+    int x=3 , y=2 , z=1 ;
+    printf("%d %d %d" , x,y,z);
+    printf("\n");
+    printf("x=%d , y=%d , z=%d", x , y, z);
+    }
