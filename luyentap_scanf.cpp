e3cc7111557d444dc375b45cd88ad0ada544b63f
HUY <huy2543ck@gmail.com>
2026-09-21 21:55:31 +0700

    Add files via upload

---
+++ luyentap_scanf.cpp
@@ -0,0 +1,62 @@
+#include<stdio.h>
+//int main(){
+//	int a;
+//	printf("nhap gia tri a:");
+//	scanf ("%d", &a);
+//	printf("gia tri cua a la: %d",a );
+//}
+//
+//
+//int main(){
+//	char ten[1] ;
+//	int tuoi ;
+//	float chieu_cao ; 
+//	printf("nhap ten cua ban: ");
+//	scanf("%s", &ten);
+//	printf("nhap tuoi cua ban : ");
+//	scanf (" %d", &tuoi);
+//	printf("nhap chieu cao cua ban:");
+//	scanf("%f", &chieu_cao);
+//	printf("ban ten la : %s \nban nam nay %d tuoi \nban cao : %.2fm", ten , tuoi , chieu_cao);
+//}
+//
+//int main(){
+//	char a[10];
+//	scanf("%s", &a);
+//	printf("%s",a);
+//}
+//
+//int main(){
+//	char ten1[10];
+//	char ten2[10];
+//	printf("nhap ten ban( khong cach nhe):");
+//	scanf("%s", &ten1);
+//	printf("nhap ten ny ban( khong cach nhe):");
+//	scanf("%s", ten2);
+//	printf("chuc ban %s va %s tram nam hanh phuc", ten1 , ten2);
+//	
+//}
+// viet chuong trinh cho khach nhap a va b , sau do tinh tong cua 2 so do
+//int main(){
+//	int a ;
+//	int b ;
+//	printf ("nhap so a : ");
+//	scanf("%d", &a);
+//	printf("nhap so b: ");
+//	scanf("%d", &b);
+//	int c = a + b ;
+//	printf("tong cua a va b = %d",c);
+//	
+//}
+// viet ctr nhap ten va tuoi roi in ra gioi thieu
+//int main (){
+//	char ten[10];
+//	int tuoi ;
+//	printf("nhap ten cua ban : ");
+//	scanf("%s", &ten);
+//	printf("nhap tuoi cua ban : ");
+//	scanf("%d", &tuoi);
+//	printf("toi ten la %s \nnam nay toi %d tuoi ", ten , tuoi); 
+//}
+
+
